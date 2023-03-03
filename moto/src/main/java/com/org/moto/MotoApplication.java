eb03e3b0bc872a0f29cebd907d05fff2c4f927ba
Joel DR <joel_la@outlook.es>
2023-03-03 13:58:52 -0500

    creacion de servicio Moto

---
+++ moto/src/main/java/com/org/moto/MotoApplication.java
@@ -0,0 +1,13 @@
+package com.org.moto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoApplication.class, args);
+	}
+
+}
---
+++ moto/src/test/java/com/org/moto/MotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.moto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
