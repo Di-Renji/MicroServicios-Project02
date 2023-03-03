2c9495e5130f9c6716ddeb67500781c3f98f9051
Joel DR <joel_la@outlook.es>
2023-03-03 13:27:36 -0500

    Creacion de servicio Carro

---
+++ carro/src/main/java/com/org/carro/CarroApplication.java
@@ -0,0 +1,13 @@
+package com.org.carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroApplication.class, args);
+	}
+
+}
---
+++ carro/src/test/java/com/org/carro/CarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
